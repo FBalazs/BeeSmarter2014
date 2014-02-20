e8b5e8c80d6be36eb25b5b69483ab74346d94bf9
Balázs Fehér <fbalazs369@gmail.com>
2014-02-20 16:04:03 +0100

    Cleared code

--- src/hu/hundevelopers/beesmarter/BeeProcess.java
+++ src/hu/hundevelopers/beesmarter/BeeProcess.java
@@ -14,7 +14,7 @@ public class BeeProcess extends Thread
 	public boolean isRunning;
 	public int requestedUps, ups, rps, width, height;
 	private int cups, crps;
-	private long lastUpdateTime, lastClockTime, tickStartTime;
+	private long lastUpdateTime, lastClockTime;
 	public float partialTick;
 	
 	public BeeProcess(SurfaceHolder holder)
@@ -24,27 +24,14 @@ public class BeeProcess extends Thread
 		this.lastUpdateTime = -1;
 	}
 	
-	float y, my, ay, r;
-	
 	public void init()
 	{
-		this.y = this.height*0.75F;
-		this.my = 0F;
-		this.ay = -this.height*9.81F/50F;
-		this.r = this.height/100F;
+		
 	}
 	
 	public void update()
 	{
-		if(this.lastUpdateTime == -1)
-			this.init();
-		this.y += this.my;
-		this.my += this.ay/this.requestedUps;
-		if(this.y <= this.r && this.my < 0F)
-		{
-			this.my *= -0.75F;
-			this.y = this.r;
-		}
+		
 	}
 	
 	public void render()
@@ -52,9 +39,11 @@ public class BeeProcess extends Thread
 		this.canvas.drawColor(Color.BLACK);
 		Paint paint = new Paint();
 		paint.setARGB(255, 255, 0, 0);
-		this.canvas.drawCircle(this.width/2, this.height-this.y-this.my*this.partialTick, 25, paint);
-		paint.setARGB(255, 0, 255, 0);
+		
+		
+		
 		paint.setTextSize(15);
+		paint.setARGB(255, 0, 255, 0);
 		this.canvas.drawText("ups: "+this.ups, 10, 25, paint);
 		this.canvas.drawText("rps: "+this.rps, 10, 50, paint);
 	}
@@ -79,14 +68,13 @@ public class BeeProcess extends Thread
 				Log.d("INFO", "ups: "+this.ups+" rps: "+this.rps);
 			}
 			
-			this.tickStartTime = System.currentTimeMillis();
-			this.partialTick = (this.tickStartTime-this.lastUpdateTime)*this.requestedUps/1000F;
+			this.partialTick = (System.currentTimeMillis()-this.lastUpdateTime)*this.requestedUps/1000F;
 			if(this.partialTick >= 1F)
 			{
 				this.cups++;
 				this.update();
 				this.partialTick = 0F;
-				this.lastUpdateTime = this.tickStartTime;
+				this.lastUpdateTime = System.currentTimeMillis();
 			}
 			this.crps++;
 			this.canvas = this.holder.lockCanvas();
