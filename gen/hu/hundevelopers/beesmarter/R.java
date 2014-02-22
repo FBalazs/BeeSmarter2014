150005ccf2c5f1105a1a5027dfb9191efcbaf75a
Balázs Fehér <fbalazs369@gmail.com>
2014-02-22 21:38:07 +0100

    Changed icon

--- gen/hu/hundevelopers/beesmarter/R.java
+++ gen/hu/hundevelopers/beesmarter/R.java
@@ -24,11 +24,12 @@ public final class R {
         public static final int change=0x7f020000;
         public static final int delete=0x7f020001;
         public static final int ic_launcher=0x7f020002;
-        public static final int move=0x7f020003;
-        public static final int move_icon=0x7f020004;
-        public static final int rotate=0x7f020005;
-        public static final int rotate2_icon=0x7f020006;
-        public static final int rotate_icon=0x7f020007;
+        public static final int logo=0x7f020003;
+        public static final int move=0x7f020004;
+        public static final int move_icon=0x7f020005;
+        public static final int rotate=0x7f020006;
+        public static final int rotate2_icon=0x7f020007;
+        public static final int rotate_icon=0x7f020008;
     }
     public static final class id {
         public static final int action_settings=0x7f080000;
